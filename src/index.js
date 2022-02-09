d435949d0656e9b2dfe1beed029051097a547470
AlexNghiem <alexandernghiem@gmail.com>
2022-02-09 15:50:01 -0800

    working react source code for initial gh-pages

--- src/index.js
+++ src/index.js
@@ -4,9 +4,32 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const data = [{
+    id: 0,
+    name: "Alex Nghiem",
+    email: "anghiem@hmc.edu",
+    phone: "(206) 669-7663"
+},{
+    id: 1,
+    name: "Stephanie Lee",
+    email: "sjlee@fhcrc.org",
+    phone: "(206) 658-7976"
+}];
+
+const dataElems = data.map(d =>
+    <div key={d.id}>
+        <span><b>{d.name}</b></span>
+        <span className="monospace">{d.email}</span>
+        <span>{d.phone}</span>
+    </div>
+);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <span><b>People</b></span>
+      <br/>
+      {dataElems}
+    {/*<App />*/}
   </React.StrictMode>,
   document.getElementById('root')
 );
